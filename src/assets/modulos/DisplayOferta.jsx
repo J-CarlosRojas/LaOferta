23781fb6286aabf370c2c21d5a53e6d8455dc8a4
J Carlos Rojas <j.carlosrojas@outlook.com>
2024-03-11 23:08:02 -0500

    add: Componenete DisplayOferta

---
+++ src/assets/modulos/DisplayOferta.jsx
@@ -0,0 +1,11 @@
+import React, { Children } from "react";
+
+const DisplayOferta = ({oferta}) =>{
+    return(
+        <div>
+            <p>{oferta}</p>
+        </div>
+    )
+}
+
+export default DisplayOferta;
