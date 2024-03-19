f3b51eaa8de011dbbe2446cdae8ae55f7a1a0a77
J Carlos Rojas <j.carlosrojas@outlook.com>
2024-03-19 07:46:36 -0500

    Comentario

--- src/assets/modulos/DisplayOferta.jsx
+++ src/assets/modulos/DisplayOferta.jsx
@@ -2,6 +2,7 @@ import React, { Children } from "react";
 
 const DisplayOferta = ({oferta}) =>{
     return(
+        //Muestra la oferta de la maquina.
         <div>
             <p>{oferta}</p>
         </div>
