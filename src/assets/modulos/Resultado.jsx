ed2d5af7e9587d2d1dc6c65f7f8b6158be271bb2
J Carlos Rojas <j.carlosrojas@outlook.com>
2024-03-28 21:42:22 -0500

    fix: nuevos props para resultado

--- src/assets/modulos/Resultado.jsx
+++ src/assets/modulos/Resultado.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Boton from "./Boton";
 
-const Resultado = ({ cambiarModal, modal }) => {
+const Resultado = ({ cambiarModal, modal, oferta, premio }) => {
   if (!modal) return null;
+
+  const premioTexto =
+    premio !== null ? "El premio es: " + premio : "No hay premio aún";
 
   return (
     <div className=" fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-50">
       <div className="bg-white p-8 rounded-lg shadow-lg ">
-        <h2>Modal abierto</h2>
+        <p>{premioTexto}</p>
+        <p>La oferta es: {oferta}</p>
         <Boton onClick={cambiarModal} texto={"Cerrar Modal"} />
       </div>
     </div>
