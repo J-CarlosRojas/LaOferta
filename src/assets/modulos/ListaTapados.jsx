80c48b7676b4d3c51aa842188a9721afe00f10d1
J Carlos Rojas <j.carlosrojas@outlook.com>
2024-04-04 22:17:41 -0500

    estilos de lista de  tapados

--- src/assets/modulos/ListaTapados.jsx
+++ src/assets/modulos/ListaTapados.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 const ListaTapados = ({ premiosRestantes }) => {
   return (
-    <div className="bg-amarillo max-w-46 pt-6 " >
-      <p className=" font-principal  text-cyan-800 text-2xl " >Premios Restantes</p>
+    <div className="bg-amarillo max-w-46 pt-6 ">
+      <p className=" font-principal  text-cyan-800 text-2xl ">
+        Premios Restantes
+      </p>
       <div></div>
       <ul>
         {premiosRestantes.map((premio, index) => (
-          <li key={index}>${premio}</li>
+          <li key={index} className=" font-secundaria text-rojo mt-1 ">
+            ${premio}
+          </li>
         ))}
       </ul>
     </div>
