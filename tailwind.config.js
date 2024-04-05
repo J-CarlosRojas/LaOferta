57613d3ac8875ea269f0563daaebe0ff3a1ca90d
J Carlos Rojas <j.carlosrojas@outlook.com>
2024-04-04 20:44:12 -0500

    configuracion de tailwind, fuentes y paleta de colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [   "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+   
+    extend: {
+      fontFamily: {
+        principal: ["Shrikhand", "serif"],
+        secundaria: ["Caprasimo", "serif"],
+        numeros: ["Chicle", "serif"],
+        carteles: ["Rum Raisin", "sans-serif"],
+      },
+      colors: {
+        crema: "#F2E0C9",
+        naranja: "#D97941",
+        rojo: "#BF4A30",
+        amarillo: "#F2A444",
+      },
+    },
   },
   plugins: [],
-}
-
+};
